4b0cec62bbdffe00518901f9754a51aeb41e5450
glebfra <gleb.frantsuzov@yandex.ru>
2024-07-31 18:28:49 +0300

    added outline server

--- src/outline/Outline.py
+++ src/outline/Outline.py
@@ -1,13 +1,7 @@
 import os
 
-from dotenv import load_dotenv
 from outline_vpn.outline_vpn import OutlineVPN
-from sqlalchemy import create_engine, select, delete
-from sqlalchemy.orm import Session
-
-from src.models.OutlineKey import OutlineKey
-
-load_dotenv('../../.env')
+from sqlalchemy import create_engine
 
 
 class Outline:
@@ -17,21 +11,6 @@ class Outline:
     def __init__(self):
         self.client = OutlineVPN(self.api_url, self.api_cert)
         self.engine = create_engine(os.getenv('SERVER_DB'), echo=True)
-
-    def add_key(self, name: str):
-        key = self.client.create_key(name=name)
-        model_key = OutlineKey(
-            id=key.key_id,
-            name=key.name,
-            password=key.password,
-            port=key.port,
-            method=key.method,
-            access_url=key.access_url
-        )
-
-        with Session(self.engine) as session:
-            session.add(model_key)
-            session.commit()
 
     def bytes_to_gbytes(self, bytes):
         return bytes / (1024 ** 3)
--- src/outline/Server.py
+++ src/outline/Server.py
@@ -1,4 +1,15 @@
+import os
+
 from fastapi import FastAPI
 
-OUTLINE_SALT = '123'
 app = FastAPI()
+
+
+@app.get('/')
+async def index():
+    return {'message': 'Hello World!', 'salt': os.getenv('OUTLINE_SALT')}
+
+
+@app.get('/conf/%s{hex_id}' % os.getenv('OUTLINE_SALT'))
+async def handle_connection(hex_id: str):
+    return {'id': hex_id}
