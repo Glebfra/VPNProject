341b1b40b8af58d81b278e67f986a1342671a626
glebfra <gleb.frantsuzov@yandex.ru>
2024-07-31 20:55:50 +0300

    added TelegramBot

--- src/bot/TelegramBot.py
+++ src/bot/TelegramBot.py
@@ -6,9 +6,9 @@ from telebot.async_telebot import AsyncTeleBot
 bot = AsyncTeleBot(os.getenv('BOT_TOKEN'))
 
 
-@bot.message_handler(commands=[''])
+@bot.message_handler(commands=['start'])
 async def welcome(message):
-    text = 'Hello World!'
+    text = 'Привет! Я бот, который раздает ключи. Напиши /key, чтобы получить ключ.'
     await bot.reply_to(message, text)
 
 
