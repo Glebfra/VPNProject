9cd3c36fb0202d96dab28e6f6fda3b6a4417a3ef
glebfra <gleb.frantsuzov@yandex.ru>
2024-07-31 15:24:46 +0300

    added creating table script

--- src/models/OutlineKey.py
+++ src/models/OutlineKey.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, String
+from sqlalchemy import DateTime, String, create_engine
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.models.Base import Base
@@ -12,3 +12,8 @@ class OutlineKey(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     key: Mapped[str] = mapped_column(String(30))
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.utcnow)
+
+
+if __name__ == '__main__':
+    engine = create_engine('postgresql+psycopg2://root:root@localhost/db', echo=True)
+    OutlineKey.__table__.create(engine)
